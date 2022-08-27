5691643660d7ca782c92ed9da8b76f0203d3a02b
Reda-Ghost <86271659+Reda-Ghost@users.noreply.github.com>
2022-08-27 17:16:32 +0000

    final project

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import data from './data';
+
+function App() {
+	let [count, setCount] = useState(0);
+	const [text, setText] = useState([]);
+
+	const handelSubmit = (e) => {
+    e.preventDefault();
+    if (count <= 0) {
+      count = 1
+    } else if (count > data.length) {
+      count = data.length
+    }
+    setText(data.slice(0 , count))
+	};
+
+	return (
+		<section className="section-center">
+			<h3>tired of boring lorem ipsum</h3>
+			<form className="lorem-form" onSubmit={handelSubmit}>
+				<label htmlFor="amount">Paragraphs:</label>
+				<input
+					type="number"
+					value={count}
+					onChange={(e) => setCount(parseInt(e.target.value))}
+				/>
+				<button type="submit" className="btn">
+					generate
+				</button>
+			</form>
+			<article className="lorem-text">
+				{text.map((item, index) => {
+					return <p key={index}>{item}</p>;
+				})}
+			</article>
+		</section>
+	);
+}
+
+export default App;
